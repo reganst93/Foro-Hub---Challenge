2615cfdfc5b7e9b4e3a2454b1aafde0cb9b55ba8
Reganst93 <stmi.93@gmail.com>
2025-08-08 22:20:46 -0400

    primer paso

---
+++ src/main/java/com/alura/Foro/Hub_Challenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alura.Foro.Hub_Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/Foro/Hub_Challenge/ForoHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.Foro.Hub_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
